c23d6f52899a58bcf029794511467d11105c3d50
Tyler Fanelli <tfnll@protonmail.com>
2024-01-05 00:47:50 -0500

    kern/start: Further document start()
    Signed-off-by: Tyler Fanelli <tfnll@protonmail.com>

--- kernel/start.c
+++ kernel/start.c
@@ -10,10 +10,10 @@ void timerinit();
 // entry.S needs one stack per CPU.
 __attribute__ ((aligned (16))) char stack0[4096 * NCPU];
 
-// a scratch area per CPU for machine-mode timer interrupts.
+// A scratch area per CPU for machine-mode timer interrupts.
 uint64 timer_scratch[NCPU][5];
 
-// assembly code in kernelvec.S for machine-mode timer interrupt.
+// Assembly code in kernelvec.S for machine-mode timer interrupt.
 extern void timervec();
 
 // entry.S jumps here in machine mode on stack0.
@@ -29,7 +29,7 @@ start()
    *
    * Some tasks that are performed below:
    *	1) mret views the mstatus register to understand which mode it is
-   *       returning to. Therefore, the bitmasks indicating supervisor mode must
+   *       returning to. Therefore, the bitmask indicating supervisor mode must
    *	   written to the mstatus register.
    *
    *	2) To view where the CPU should jump to on mret, the Machine Exception
@@ -49,33 +49,72 @@ start()
    *	7) Store the hardware thread's ID in it's tp register.
    */
 
-  // set M Previous Privilege mode to Supervisor, for mret.
+  /*
+   * Set M Previous Privilege mode to Supervisor. When mret is executed, the
+   * processor will switch into supervisor mode.
+   */
   mstatus = r_mstatus();
-  mstatus &= ~MSTATUS_MPP_MASK;	// Clear the previous privilege mode bit(s).
-  mstatus |= MSTATUS_MPP_S;		// Set the MPP mode to supervisor.
-  w_mstatus(mstatus);                // Write new MPP mode (supervisor) to mstatus.
+  mstatus &= ~MSTATUS_MPP_MASK; // Clear the previous privilege mode bit(s).
+  mstatus |= MSTATUS_MPP_S;	// Set the MPP mode to supervisor.
+  w_mstatus(mstatus);		// Write new MPP mode (supervisor) to mstatus.
 
-  // set M Exception Program Counter to main, for mret.
-  // requires gcc -mcmodel=medany
+  /*
+   * Set M Exception Program Counter to main. When mret is executed, the
+   * processor will jump to main()'s address, effectively beginning to run main
+   * in superviosr mode.
+   * requires gcc -mcmodel=medany
+   */
   w_mepc((uint64)main);
 
-  // disable paging for now.
+  /*
+   * Disable paging for now. The satp register holds the address of the
+   * supervisor mode page table. Writing 0 to this register effectively disables
+   * paging in supervisor mode.
+   */
   w_satp(0);
 
-  // delegate all interrupts and exceptions to supervisor mode.
+  /*
+   * Delegate all interrupts and exceptions to supervisor mode. The medeleg and
+   * mideleg registers hold bitflags indicating which exception or interrupt
+   * will be handled by supervisor mode. Since these registers are 32 bits in
+   * size, setting their values to 0xffff effectively indicates that all
+   * exceptions and interrupts will be handled in supervisor mode.
+   *
+   * The sie register holds interrupt-enable bits. There are three types of
+   * interrupts:
+   * - Software interrupt (SSIE)
+   * - Timer interrupt (STIE)
+   * - External interrupt (SEIE)
+   *
+   * By setting each of the interrupt's bitflag in the sie register, each type
+   * of interrupt will be enabled in supervisor mode.
+   */
   w_medeleg(0xffff);
   w_mideleg(0xffff);
   w_sie(r_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
 
-  // configure Physical Memory Protection to give supervisor mode
-  // access to all of physical memory.
+  /*
+   * Configure Physical Memory Protection to give supervisor mode access to all
+   * of physical memory.
+   *
+   * The PMP address registers are 16 CSRs named pmpaddr0-pmpaddr15. Each PMP
+   * address register encodes bits 52-55 of a 56-bit physical address. Not all
+   * physical address bits may be implemented, and so the pmpaddr registers are
+   * WARL.
+   *
+   * TODO: How do the PMP address and PMP configuration registers work together?
+   */
   w_pmpaddr0(0x3fffffffffffffull);
   w_pmpcfg0(0xf);
 
-  // ask for clock interrupts.
+  /*
+   * Ask for clock interrupts.
+   */
   timerinit();
 
-  // keep each CPU's hartid in its tp register, for cpuid().
+  /*
+   * Keep each CPU's hartid in its tp register, for cpuid().
+   */
   int id = r_mhartid();
   w_tp(id);
 
@@ -83,35 +122,50 @@ start()
   asm volatile("mret");
 }
 
-// set up to receive timer interrupts in machine mode,
-// which arrive at timervec in kernelvec.S,
-// which turns them into software interrupts for
-// devintr() in trap.c.
+/*
+ * Set up the current CPU to receive timer interrupts in machine mode, which
+ * arrive at timervec in kernelvec.S, which in-turn turns them into software
+ * interrupts for devintr() in trap.c.
+ */
 void
 timerinit()
 {
-  // each CPU has a separate source of timer interrupts.
+  // Each CPU has a separate source of timer interrupts.
   int id = r_mhartid();
 
-  // ask the CLINT for a timer interrupt.
-  int interval = 1000000; // cycles; about 1/10th second in qemu.
-  *(uint64*)CLINT_MTIMECMP(id) = *(uint64*)CLINT_MTIME + interval;
+  /*
+   * Ask the CLINT for a timer interrupt.
+   *
+   * Platforms provide a real-time counter, exposed as a memory-mapped
+   * machine-mode register, mtime. Platforms also provide a 64-bit memory-mapped
+   * machine-mode timer compare register, mtimecmp, which causes a timer
+   * interrupt to be posted when the mtime register contains a value greater
+   * than or equal to the value in the mtimecmp register.
+   *
+   * Set this CPU's mtimecmp register to to be 1000000 cycles more than the
+   * current global mtime register, in-effect setting the timer to interrupt
+   * every 1000000 cycles.
+   */
+  int interval = 1000000;	// Cycles (about 1/10th second in qemu).
+  *(uint64 *) CLINT_MTIMECMP(id) = *(uint64 *) CLINT_MTIME + interval;
 
-  // prepare information in scratch[] for timervec.
-  // scratch[0..2] : space for timervec to save registers.
-  // scratch[3] : address of CLINT MTIMECMP register.
-  // scratch[4] : desired interval (in cycles) between timer interrupts.
+  /*
+   * Prepare information in scratch[] for timervec.
+   * scratch[0..2]: Space for timervec to save registers.
+   * scratch[3]: Address of CLINT MTIMECMP register.
+   * scratch[4]: Desired interval (in cycles) between timer interrupts.
+   */
   uint64 *scratch = &timer_scratch[id][0];
   scratch[3] = CLINT_MTIMECMP(id);
   scratch[4] = interval;
-  w_mscratch((uint64)scratch);
+  w_mscratch((uint64) scratch);
 
-  // set the machine-mode trap handler.
-  w_mtvec((uint64)timervec);
+  // Set the machine-mode trap handler.
+  w_mtvec((uint64) timervec);
 
-  // enable machine-mode interrupts.
+  // Enable machine-mode interrupts.
   w_mstatus(r_mstatus() | MSTATUS_MIE);
 
-  // enable machine-mode timer interrupts.
+  // Enable machine-mode timer interrupts.
   w_mie(r_mie() | MIE_MTIE);
 }
